e5022cdd9f03d3b3d9cb6e1de18a11ab8777dfba
NiamYoseph <liamcorbett97@gmail.com>
2022-06-07 11:15:45 -0400

    Pull schedule from MBTA API

--- src/components/DepartureBoard.tsx
+++ src/components/DepartureBoard.tsx
@@ -1,3 +1,5 @@
+import ky from 'ky';
+import { useEffect, useState } from 'react';
 import { Table } from '@mantine/core';
 
 interface DepartureBoardProps {
@@ -74,13 +76,64 @@ export interface IncludedAttributes {
 
 // Component
 const DepartureBoard = (props: DepartureBoardProps) => {
-    const listings = <tr>
-        <td>Test</td>
-        <td>Test</td>
-        <td>Test</td>
-        <td>Test</td>
-        <td>Test</td>
-    </tr>;
+    const [schedules, setSchedules] = useState<Schedule[]>();
+    const [predictions, setPredictions] = useState<Included[]>();
+
+    useEffect(() => {
+
+        async function fetchSchedulesAndPredictions() {
+
+            // TODO: Make URL Dynamic
+            const response: SchedulesResponse = await ky.get(
+                'https://api-v3.mbta.com/schedules?sort=departure_time&include=prediction&filter%5Bdate%5D=2022-06-07&filter%5Broute_type%5D=2&filter%5Bmin_time%5D=09%3A05&filter%5Bstop%5D=place-north'
+                ).json();
+
+            if (response) {
+
+                // Remove schedules with null departure times
+                const filteredSchedules = response.data.filter((schedule) => {
+                    return schedule.attributes.departure_time !== null;
+                });
+
+                // Remove predictions with null departure times
+                const filteredPredictions = response.included.filter((prediction) => {
+                    return prediction.attributes.departure_time !== null;
+                });
+
+                console.log({schedules: filteredSchedules});
+                console.log({predictions: response.included});
+                console.log({filteredPredictions: filteredPredictions});
+
+                setSchedules(filteredSchedules);
+                setPredictions(filteredPredictions);
+            }
+        }
+
+        fetchSchedulesAndPredictions();
+        
+        // refresh the board every 60 seconds
+        const interval = setInterval(() => {
+            fetchSchedulesAndPredictions();
+        }, 60000);
+        return () => clearInterval(interval);
+
+    }, [props.station]);
+    
+    let listings;
+
+    if (schedules) {
+        listings = schedules.map((schedule) => {
+            const timestring = new Date( schedule.attributes.departure_time! ).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+
+            return <tr key={schedule.id}>
+                <td>{schedule.attributes.departure_time ? timestring : "null"}</td>
+                <td>{schedule.relationships.route.data?.id.split('-')[1]}</td>
+                <td>{}</td>                           {/* TODO: Add Vehicle # Determination */}
+                <td>TBD</td>                          {/* TODO: Add Track # Determination */}
+                <td>{}</td>
+            </tr>
+        });
+    }
 
     return <Table>
         <thead>
