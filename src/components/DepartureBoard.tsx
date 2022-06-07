70d1161e9f04c5825de12d3fa7e5c8666d7642a7
NiamYoseph <liamcorbett97@gmail.com>
2022-06-07 15:06:54 -0400

    Fix issue with single-digit hours

--- src/components/DepartureBoard.tsx
+++ src/components/DepartureBoard.tsx
@@ -57,6 +57,7 @@ export enum DataType {
     Vehicle = "vehicle",
 }
 
+// Should read as "prediction" but is named this way for consistency with API
 export interface Included {
     attributes:    IncludedAttributes;
     id:            string;
@@ -83,9 +84,6 @@ interface DynamicSchedule {
     status: string;
 }
 
-
-
-// Component
 const DepartureBoard = (props: DepartureBoardProps) => {
     const [dynamicSchedules, setDynamicSchedules] = useState<DynamicSchedule[]>();
 
@@ -94,14 +92,15 @@ const DepartureBoard = (props: DepartureBoardProps) => {
         async function fetchDynamicSchedules() {
 
             const now = new Date();
-            const todayIso = now.toISOString().substring(0,10);
+            const fiveMinutesAgo = new Date();
+            fiveMinutesAgo.setMinutes(now.getMinutes() - 5);
 
             const url = new URL('https://api-v3.mbta.com/schedules');
             url.searchParams.append('sort', 'departure_time');
             url.searchParams.append('include', 'prediction');
-            url.searchParams.append('filter[date]', todayIso);
+            url.searchParams.append('filter[date]', now.toISOString().substring(0,10));
             url.searchParams.append('filter[route_type]', '2');
-            url.searchParams.append('filter[min_time]', now.getHours() + ":" + (now.getMinutes() - 5));
+            url.searchParams.append('filter[min_time]', fiveMinutesAgo.toLocaleString('en-US', {hour12: false, hour: '2-digit', minute: '2-digit'}));
             url.searchParams.append('filter[stop]', props.station);
 
             const response: SchedulesResponse = await ky.get(url).json();
@@ -131,6 +130,7 @@ const DepartureBoard = (props: DepartureBoardProps) => {
 
                         // If the mapped schedule has a related prediction
                         if (schedule.relationships.prediction!.data) {
+
                             // Find the full prediction data from the "includes" property of the response
                             const prediction = filteredPredictions.find((prediction) => {
                                 return prediction.id === schedule.relationships.prediction!.data!.id;
