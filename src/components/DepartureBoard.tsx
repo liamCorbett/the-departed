b1aa521eb215ab907c9968460e72d132a681c078
NiamYoseph <liamcorbett97@gmail.com>
2022-06-07 21:47:15 -0400

    Split DynamicSchedule generation into own file

--- src/components/DepartureBoard.tsx
+++ src/components/DepartureBoard.tsx
@@ -1,6 +1,7 @@
 import ky from 'ky';
 import { useEffect, useState } from 'react';
 import { Select, Table } from '@mantine/core';
+import makeDynamic from '../core/makedynamic';
 
 interface DepartureBoardProps {
     station: string;
@@ -76,7 +77,7 @@ export interface IncludedAttributes {
 }
 
 // Schedule which substitutes in prediction data where possible
-interface DynamicSchedule {
+export interface DynamicSchedule {
     id: string;
     departureTime: string;
     destination: string;
@@ -124,59 +125,7 @@ const DepartureBoard = (props: DepartureBoardProps) => {
                     });
                 }
 
-                setDynamicSchedules(
-                    filteredSchedules.slice(0, props.rows ? props.rows : 10).map((schedule) => {
-
-                        // Timestring in HH:MM format
-                        let timestring = new Date(schedule.attributes.departure_time!)
-                                            .toLocaleString('en-US', { 
-                                                    hour: 'numeric', 
-                                                    minute: 'numeric',
-                                                    hour12: true 
-                                                });
-
-                        // If the mapped schedule has a related prediction
-                        if (schedule.relationships.prediction!.data) {
-
-                            // Find the full prediction data from the "includes" property of the response
-                            const prediction = filteredPredictions.find((prediction) => {
-                                return prediction.id === schedule.relationships.prediction!.data!.id;
-                            });
-
-                            // Use the prediction's departure time
-                            if (prediction!.attributes.departure_time) {
-                                timestring = new Date(prediction!.attributes.departure_time!)
-                                                .toLocaleString('en-US', { 
-                                                    hour: 'numeric', 
-                                                    minute: 'numeric',
-                                                    hour12: true 
-                                                });
-                            }
-
-                            return {
-                                id: schedule.id,
-                                departureTime: timestring,
-                                destination: schedule.relationships.route.data ? schedule.relationships.route.data.id.split('-')[1] : '',
-                                trainNum: prediction!.relationships.vehicle!.data ? prediction!.relationships.vehicle!.data.id : '',
-                                trackNum: prediction!.relationships.stop.data ? prediction!.relationships.stop.data.id.split('-')[2] : 'TBD',
-                                status: prediction!.attributes.status ? prediction!.attributes.status : 'On Time',
-                            }
-
-                        }
-
-
-                        return {
-                            id: schedule.id,
-                            departureTime: timestring,
-                            destination: schedule.relationships.route.data ? schedule.relationships.route.data.id.split('-')[1] : '',
-                            trainNum: '',
-                            trackNum: 'TBD',
-                            status: 'On Time',
-                        };
-                    })
-                );
-
-
+                setDynamicSchedules(makeDynamic(filteredSchedules, filteredPredictions));
                 
                 console.log({schedules: response.data});
                 console.log({filteredSchedules: filteredSchedules});
---
+++ src/core/makedynamic.ts
@@ -0,0 +1,57 @@
+import { Schedule, Included, DynamicSchedule } from "../components/DepartureBoard";
+
+const makeDynamic = (schedules: Schedule[], predictions: Included[]): DynamicSchedule[] => {
+
+    return schedules.map((schedule) => {
+
+        // Timestring in HH:MM format
+        let timestring = new Date(schedule.attributes.departure_time!)
+                            .toLocaleString('en-US', { 
+                                    hour: 'numeric', 
+                                    minute: 'numeric',
+                                    hour12: true 
+                                });
+    
+        // If the mapped schedule has a related prediction
+        if (schedule.relationships.prediction!.data) {
+    
+            // Find the full prediction data from the "includes" property of the response
+            const prediction = predictions.find((prediction) => {
+                return prediction.id === schedule.relationships.prediction!.data!.id;
+            });
+    
+            // Use the prediction's departure time
+            if (prediction!.attributes.departure_time) {
+                timestring = new Date(prediction!.attributes.departure_time!)
+                                .toLocaleString('en-US', { 
+                                    hour: 'numeric', 
+                                    minute: 'numeric',
+                                    hour12: true 
+                                });
+            }
+    
+            return {
+                id: schedule.id,
+                departureTime: timestring,
+                destination: schedule.relationships.route.data ? schedule.relationships.route.data.id.split('-')[1] : '',
+                trainNum: prediction!.relationships.vehicle!.data ? prediction!.relationships.vehicle!.data.id : '',
+                trackNum: prediction!.relationships.stop.data ? prediction!.relationships.stop.data.id.split('-')[2] : 'TBD',
+                status: prediction!.attributes.status ? prediction!.attributes.status : 'On Time',
+            }
+    
+        }
+    
+    
+        return {
+            id: schedule.id,
+            departureTime: timestring,
+            destination: schedule.relationships.route.data ? schedule.relationships.route.data.id.split('-')[1] : '',
+            trainNum: '',
+            trackNum: 'TBD',
+            status: 'On Time',
+        };
+    })
+
+}
+
+export default makeDynamic;
